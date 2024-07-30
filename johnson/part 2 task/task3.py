847281f57c41af133fa90f29053c6170c0252be9
johnson <ibitoyejohnson234@gmail.com>
2024-07-30 11:56:30 +0100

    commit message

--- johnson/part 2 task/task3.py
+++ johnson/part 2 task/task3.py
@@ -1,7 +1,21 @@
 def add_strings(num1, num2):
     return str(int(num1) + int(num2))
 result = add_strings("123", "456")
-print(result)  
+
+
+
+
+
+
+def check_even_odd(number):
+    if number % 2 == 0:
+        return "It is an even number"
+    else:
+        return "It is an odd number"
+
+
+print(result)
+print(check_even_odd(9))  
 
 
 
